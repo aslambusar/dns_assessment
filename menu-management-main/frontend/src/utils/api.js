6a93a9024f9316d1a8c5e0d34f71f5206f714c19
aslambusar <aslambusar@gmail.com>
2025-01-31 03:03:06 +0530

    Update api.js

--- menu-management-main/frontend/src/utils/api.js
+++ menu-management-main/frontend/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'http://localhost:5000/api', 
-  baseURL: 'https://dns-assessment-frontend4.onrender.com', 
+  baseURL: 'https://dns-assessment-backend1.onrender.com', 
 
 });
 
